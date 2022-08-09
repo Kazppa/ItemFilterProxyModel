4e366d62514fa0f83e1f4e397b37ad808f1fa560
Mathieu <mathieu.lembke@gmail.com>
2022-08-09 14:04:25 +0200

    Row deletion is now fully functional

--- example/ExampleTreeView.cpp
+++ example/ExampleTreeView.cpp
@@ -2,6 +2,7 @@
 
 #include <QMenu>
 #include <QScrollBar>
+#include <QStack>
 
 ExampleTreeView::ExampleTreeView(QWidget *parent) :
         QTreeView(parent)
@@ -17,10 +18,10 @@ ExampleTreeView::ExampleTreeView(QWidget *parent) :
     m_menu = new QMenu(this);
     m_expandAction = m_menu->addAction(tr("Expand"));
     m_collapseAction = m_menu->addAction(tr("Collapse"));
+    m_removeAction = m_menu->addAction(tr("Delete item"));
     m_menu->addSeparator();
     m_expandAllAction = m_menu->addAction(tr("Expand all"));
     m_collapseAllAction = m_menu->addAction(tr("Collapse all"));
-    m_removeAction = m_menu->addAction(tr("Delete item"));
 
     connect(m_expandAllAction, &QAction::triggered, this, &QTreeView::expandAll);
     connect(m_collapseAllAction, &QAction::triggered, this, &QTreeView::collapseAll);
@@ -52,6 +53,44 @@ void ExampleTreeView::onContextMenuRequested(const QPoint& pos)
     m_removeAction->setVisible(!selectedIndexes().empty());
 
     m_menu->popup(mapToGlobal(pos));
+}
+
+QModelIndex ExampleTreeView::searchTextIndex(const QString &searchedText) const
+{
+    auto sourceModel = model();
+    Q_ASSERT(sourceModel);
+    QStack<QModelIndex> stack;
+
+    const auto rootRowCount = sourceModel->rowCount();
+    const auto rootColumnCount = sourceModel->columnCount();
+    for (auto row = 0; row < rootRowCount; ++row) {
+        for (auto col = 0; col < rootColumnCount; ++col) {
+            const auto index = sourceModel->index(row, col);
+            const auto txt = index.data().toString();
+            if (txt == searchedText) {
+                return  index;
+            }
+            stack.push_back(index);
+        }
+    }
+
+    while(!stack.empty()) {
+        const auto parentIndex = stack.pop();
+
+        const auto rowCount = sourceModel->rowCount(parentIndex);
+        const auto colCount = sourceModel->columnCount(parentIndex);
+        for (auto row = 0; row < rowCount; ++row) {
+            for (auto col = 0; col < colCount; ++col) {
+                const auto index = sourceModel->index(row, col, parentIndex);
+                const auto txt = index.data().toString();
+                if (txt == searchedText) {
+                    return  index;
+                }
+                stack.push_back(index);
+            }
+        }
+    }
+    return {};
 }
 
 void ExampleTreeView::currentChanged(const QModelIndex &newIdx, const QModelIndex &prevIdx)
